2f26f66b92118ffcda3d17cfee963b3c3af55773
Muhamad Umer <umerdev228@gmail.com>
2021-03-01 20:33:45 +0500

    product image check changes

--- resources/js/components/front-end/categories/detailComponent.vue
+++ resources/js/components/front-end/categories/detailComponent.vue
@@ -39,7 +39,8 @@
           <div v-for="product in filteredProducts" class="col-6 mb-5" style="width: 50%; min-height: 240px; padding-right: 8px; padding-left: 8px;">
             <router-link :to="{name: 'ProductDetails', params: { id: product.id }}" class="nav-item nav-link">
               <a href="javascript:void(0)" style="color: black;">
-                <div :style="{'background': 'url(' + product.media[0].path + ') center center / contain no-repeat', 'background-size': '100px', 'border-radius': '7px', 'width': '100%', 'display': 'block', 'cursor': 'pointer', 'min-height': '240px', 'position': 'relative'}"></div>
+                <div v-if="product.media.length > 0" :style="{'background': 'url(' + product.media[0].path + ') center center / contain no-repeat', 'background-size': '100px', 'border-radius': '7px', 'width': '100%', 'display': 'block', 'cursor': 'pointer', 'min-height': '240px', 'position': 'relative'}"></div>
+                <div v-else :style="{'background': 'url(/images/product-default-img.jpg) center center / contain no-repeat', 'background-size': '100px', 'border-radius': '7px', 'width': '100%', 'display': 'block', 'cursor': 'pointer', 'min-height': '240px', 'position': 'relative'}"></div>
                 <div>
                   <p class="mt-2 mb-0 cut-text-one-line text-left pl-1" style="font-size: 14px; font-weight: bold; height: 20px;">
                     {{ $parent.language === 'ar' ? product.name_ar : product.name }}
